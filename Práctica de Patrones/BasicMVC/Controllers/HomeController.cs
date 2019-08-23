473fdd52a362f98cd013bde44d91f1be7a2dfd44
ITIC-9b-fmm <54427526+ITIC-9b-fmm@users.noreply.github.com>
2019-08-23 12:33:12 -0500

    Add files via upload

---
+++ Práctica de Patrones/BasicMVC/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using BasicMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BasicMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            /*Se crea una lista y se instancían 4 coches dándoles sus atributos 
+            correspondientes. Esta parte es la del Controlador*/
+            var coches = new List<Coche>
+            {
+                new Coche {Marca="BMW",Modelo="X6",Color="Gris"},
+                new Coche {Marca="Mercedes",Modelo="A200",Color="Blanco"},
+                new Coche {Marca="Porsche",Modelo="Cayanne",Color="Azul"},
+                new Coche {Marca="Ford",Modelo="Mustang",Color="Rojo"}
+            };
+
+            return View(coches);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Práctica de Patrones/BasicMVC/Models/Coche.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BasicMVC.Models
+{
+    /*Se ha creado esta clase como el modelo del patrón MVC
+     Esta parte es el Módelo del patrón.*/
+    public class Coche
+    {
+        //Se le añaden las propiedades a la clase Coche
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string Color { get; set; }
+    }
+}
